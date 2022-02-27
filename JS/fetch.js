0073f97601fe0cde0d75f9ec63b88cd42c7c26f7
n-ce <abrocks1120@gmail.com>
2022-02-27 18:48:48 +0530

    overhauled site architecture

--- JS/fetch.js
+++
@@ -1,8 +0,0 @@
-function fetchPage(name) {
-  fetch("Pages/" + name + ".html")
-    .then(response => response.text())
-    .then(html => {
-      document.getElementById('root').innerHTML = html;
-    });
-}
-fetchPage('landingpage');
--- JS/showHide.js
+++ JS/showHide.js
@@ -1,9 +1,10 @@
 let show = (name) => {
   if (name != 'qualifications') {
-    const allList = ['tenthRailway', 'tenthBanking'];
+    const allList = ['tenthRailway','tenthDefense'];
     for (var i = 0; i < allList.length; i++) {
       document.getElementById(allList[i]).classList.add('is-hidden');
     }
   }
   document.getElementById(name).classList.toggle('is-hidden');
 }
+show('banner');
