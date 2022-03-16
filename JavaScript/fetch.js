c0ade17e1379cc124f695070e245eae530916d4f
abrocks1120@gmail.com <abrocks1120@gmail.com>
2022-03-16 20:00:09 +0530

    fetch setup method succeed

--- JavaScript/fetch.js
+++ JavaScript/fetch.js
@@ -1,18 +1,22 @@
-function appendData(data) {
-  for (var i = 0; i < data.length; i++) {
-    var list = document.createElement("li");
-    list.innerHTML = `<u>${data[i].name}</u><br><i>"${data[i].quote}"</i>`;
-    document.getElementById('list').appendChild(list);
-  }
-}
 
 function parse(Y) {
-  return fetch("../Databases/" + Y + ".json").then(function(response) {
-    return response.json();
-  }).then(function(data) {
-    appendData(data);
-  }).catch(function(err) {
-    console.log('error: ' + err);
-  });;
+  console.log(Y);
+  fetch("../Databases/" + Y + ".json")
+    .then(res => res.json())
+    .then(data =>
+    {
+      for (var i = 0; i < data.length; i++) {
+        var list = document.createElement("li");
+        if (Y == 'testimonials') {
+          list.innerHTML = `<u>${data[i].name}</u><br><i>"${data[i].quote}"</i>`;
+        }
+        else {
+          list.innerHTML = `${data[i].name}<br>${data[i].post}<br>${data[i].salary}<br>`;
+        }
+        document.getElementById('list').appendChild(list);
+      }
+    })
+    .catch(function(err) {
+      console.log('error: ' + err);
+    });
 }
-  
