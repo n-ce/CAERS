db68afdb463d292b5b5501348f46b24ddf885e7c
abrocks1120@gmail.com <abrocks1120@gmail.com>
2022-04-29 10:58:24 +0530

    formatting code, fixed fetch multiplication

--- JavaScript/fetch.js
+++ JavaScript/fetch.js
@@ -1,25 +1,28 @@
+const ul = document.getElementById('list');
 
 function parse(Y) {
-  console.log(Y);
   fetch("../Databases/" + Y + ".json")
     .then(res => res.json())
     .then(data =>
     {
+      ul.innerHTML = null;
       for (var i = 0; i < data.length; i++) {
         var list = document.createElement("li");
         if (Y == 'testimonials') {
           list.innerHTML = `<u>${data[i].name}</u><br><i>"${data[i].quote}"</i>`;
         }
         else {
-
-          list.innerHTML = `<hgroup><h2>${data[i].name}</h2><h3><br>${data[i].post}<br>${data[i].salary}
-<br>${data[i].location}<br></h3></hgroup>`;
+          list.innerHTML =
+            `<hgroup>
+          <h2>${data[i].name}</h2>
+          <h3><br>
+          ${data[i].post}<br>
+          ${data[i].salary}<br>
+          ${data[i].location}<br>
+          </h3>
+          </hgroup>`;
         }
-
-document.getElementById('list').appendChild(list);
+        ul.appendChild(list);
       }
-    })
-    .catch(function(err) {
-      console.log('error: ' + err);
     });
 }
